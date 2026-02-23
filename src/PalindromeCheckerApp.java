74dc312d31b281f8cfcd124c1d6f376bea6f0be8
aayati-srivastava <aayati.sri@gmail.com>
2026-02-23 14:39:14 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,20 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         System.out.println("Welcome to Palindrome Checker App");
+
+        UseCase1.run();
+    }
+}
+
+class UseCase1 {
+    public static void run() {
+        System.out.println("=======================================");
+        System.out.println("      PALINDROME CHECKER APP");
+        System.out.println("=======================================");
+        System.out.println("Version : 1.0");
+        System.out.println("Author  : Aayati Srivastava");
+        System.out.println("---------------------------------------");
+        System.out.println("Welcome to the Palindrome Checker Application!");
+        System.out.println("=======================================");
     }
 }
