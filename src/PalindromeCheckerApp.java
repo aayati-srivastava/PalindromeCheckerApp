8143d56f37f1c5ec677edda290f895b08a4bacfe
aayati-srivastava <aayati.sri@gmail.com>
2026-02-23 14:44:22 +0530

    UC2:Hardcoded palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,10 +2,15 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
         System.out.println("Welcome to Palindrome Checker App");
 
+        // Run UC1
         UseCase1.run();
+
+        // Run UC2
+        UseCase2.run();
     }
 }
 
+// ---------------- UC1 ----------------
 class UseCase1 {
     public static void run() {
         System.out.println("=======================================");
@@ -15,6 +20,28 @@ class UseCase1 {
         System.out.println("Author  : Aayati Srivastava");
         System.out.println("---------------------------------------");
         System.out.println("Welcome to the Palindrome Checker Application!");
+        System.out.println("=======================================");
+    }
+}
+
+// ---------------- UC2 ----------------
+class UseCase2 {
+    public static void run() {
+        String word = "madam";  // hardcoded string
+        String result;
+
+        // Check palindrome
+        if (word.equals(new StringBuilder(word).reverse().toString())) {
+            result = word + " is a palindrome";
+        } else {
+            result = word + " is not a palindrome";
+        }
+
+        System.out.println("=======================================");
+        System.out.println("      HARDCODED PALINDROME CHECK");
+        System.out.println("=======================================");
+        System.out.println("Word: " + word);
+        System.out.println("Result: " + result);
         System.out.println("=======================================");
     }
 }
