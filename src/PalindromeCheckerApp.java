8cbbdf772b0b2fa5a0dbadd5ee499d68b9e09563
Parthcodes1 <parthcse9@gmail.com>
2026-03-02 14:19:30 +0530

    Added Palindrome Checker App UC1–UC11

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,40 @@
 public class PalindromeCheckerApp {
+
+    static boolean checkPalindrome(String text) {
+
+        // Normalize string
+        text = text.replaceAll("\\s+", "").toLowerCase();
+
+        boolean isPalindrome = true;
+
+        int start = 0;
+        int end = text.length() - 1;
+
+        // Compare characters
+        while(start < end) {
+
+            if(text.charAt(start) != text.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        return isPalindrome;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App");
+
+        String text = "A man a plan a canal Panama";
+
+        boolean result = checkPalindrome(text);
+
+        // Result
+        if(result)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not a Palindrome");
     }
 }
